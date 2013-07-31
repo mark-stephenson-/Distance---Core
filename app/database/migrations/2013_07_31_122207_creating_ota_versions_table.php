27816b1ed8a4c59e7f6e1a1f14e17a8d8971c45f
Craig Sansam <craig@craigy.co.uk>
2013-07-31 13:23:57 +0100

    Creating ota_versions table

---
+++ app/database/migrations/2013_07_31_122207_creating_ota_versions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatingOtaVersionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ota_versions', function($table)
+		{
+			$table->increments('id');
+		            $table->enum('platform', array('ios', 'android'));
+		            $table->enum('environment', array('production', 'testing'));
+		            $table->string('version');
+		            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('ota_versions');
+	}
+
+}
