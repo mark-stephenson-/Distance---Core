7b7c0f6bde9abaaf10370c6a4ccb3e8925750af7
Craig Sansam <craig@craigy.co.uk>
2013-08-02 12:51:29 +0100

    Removing a JS file

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -85,7 +85,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script> CKEDITOR_BASEPATH = '{{ URL::to('') }}/js/ckeditor/'; </script>
         <script src="/js/app.min.js"></script>
-        <script src="/js/bootstrap/bootstrap-dropdown.js"></script>
         @yield('js')
 
     </body>
