dc43901f08c20721166906c71310ab8c8961a7bd
Craig Sansam <craig@craigy.co.uk>
2013-08-07 15:07:12 +0100

    Changing date add-on to use <a> instead of <span>

--- app/views/nodecategories/date.blade.php
+++ app/views/nodecategories/date.blade.php
@@ -26,9 +26,9 @@
 
 <div id="{{ $identifier }}" class="input-append span8" style="margin-left: 0px">
     {{ Form::text('nodetype['. $column->name .']', Input::old('nodetype.' . $column->name, date('d/m/Y h:i', strtotime($value))), ['class' => 'span8 validate-date datepicker', 'data-format' => 'dd/MM/yyyy hh:mm']) }}
-    <span class="add-on">
+    <a class="btn add-on">
         <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-    </span>
+    </a>
 </div>
 
 @if ($column->description)
