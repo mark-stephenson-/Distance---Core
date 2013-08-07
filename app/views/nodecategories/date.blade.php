05435d350c9a4d7c9a525f20f1dfa2d5f6d4b818
Craig Sansam <craig@craigy.co.uk>
2013-08-07 13:32:10 +0100

    Adding date picker to admin view

--- app/views/nodecategories/date.blade.php
+++ app/views/nodecategories/date.blade.php
@@ -1,4 +1,6 @@
 <?php
+    $identifier = uniqid();
+
     if (!isset($data)) {
         if (!@$column->default) {
             $value = 'today';
@@ -10,7 +12,24 @@
     }
 ?>
 
-{{ Form::text('nodetype['. $column->name .']', Input::old('nodetype.' . $column->name, date('d/m/Y', strtotime($value))), ['class' => 'span8 validate-date datepicker']) }}
+<script src="/js/datetimepicker-0.0.11/js/bootstrap-datetimepicker.min.js"></script>
+<script src="/js/bootstrap/bootstrap-collapse.js"></script>
+
+<script type="text/javascript">
+    $(function() {
+        $('#{{ $identifier }}').datetimepicker({
+            language: 'en-GB',
+            pickSeconds: false
+        });
+    });
+</script>
+
+<div id="{{ $identifier }}" class="input-append span8">
+    {{ Form::text('nodetype['. $column->name .']', Input::old('nodetype.' . $column->name, date('d/m/Y h:i', strtotime($value))), ['class' => 'span8 validate-date datepicker', 'data-format' => 'dd/MM/yyyy hh:mm']) }}
+    <span class="add-on">
+        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+    </span>
+</div>
 
 @if ($column->description)
     <span class="help-block">{{ $column->description }}</span>
