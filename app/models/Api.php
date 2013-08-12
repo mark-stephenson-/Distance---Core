be59d9e0d65965249b0b777a5c83b5d63c3f257e
Craig Sansam <craig@craigy.co.uk>
2013-08-12 13:43:17 +0100

    Removing collection_id and status from API

--- app/models/Api.php
+++ app/models/Api.php
@@ -15,9 +15,11 @@ class Api extends \BaseModel {
             $content->created_by, // This wasn't passed in the original API
             $content->latest_revision, // Currently no version access via the API
             $content->published_revision, // Currently no version access via the API
-            $content->retired_at // If this has a value, this node shouldn't be displayed
+            $content->retired_at, // If this has a value, this node shouldn't be displayed
+            $content->collection_id,
+            $content->status
         );
-            
+
         if ( $contentType == "text/xml" ) {
             $json = $content->toJSON();
             $format = new format;
