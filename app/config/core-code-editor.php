0dc965af348a8ad59b78756d19891608cc311b98
Craig Sansam <craig@craigy.co.uk>
2013-08-12 09:58:05 +0100

    Fixing "Add JS and CSS types to Code field"

--- app/config/core-code-editor.php
+++ app/config/core-code-editor.php
@@ -1,13 +1,11 @@
 <?php
 
 return array(
-    'xml' => array(
-        'name' => 'XML',
-        'mode' => 'application/xml',
+    'css' => array(
+        'name' => 'CSS',
+        'mode' => 'text/css',
         'scripts' => array(
-            "/js/codemirror/addon/hint/xml-hint.js",
-            "/js/codemirror/mode/xml/xml.js",
-            "/js/codemirror/mode/htmlmixed/htmlmixed.js"
+            "/js/codemirror/mode/css/css.js"
         ),
     ),
 
@@ -23,12 +21,31 @@ return array(
         ),
     ),
 
+    'javascript' => array(
+        'name' => 'JavaScript',
+        'mode' => 'text/javascript',
+        'scripts' => array(
+            "/js/codemirror/addon/hint/javascript-hint.js",
+            "/js/codemirror/mode/javascript/javascript.js",
+        ),
+    ),
+
     'json' => array(
         'name' => 'JSON',
         'mode' => 'application/json',
         'scripts' => array(
             "/js/codemirror/addon/hint/javascript-hint.js",
             "/js/codemirror/mode/javascript/javascript.js",
+        ),
+    ),
+
+    'xml' => array(
+        'name' => 'XML',
+        'mode' => 'application/xml',
+        'scripts' => array(
+            "/js/codemirror/addon/hint/xml-hint.js",
+            "/js/codemirror/mode/xml/xml.js",
+            "/js/codemirror/mode/htmlmixed/htmlmixed.js"
         ),
     ),
 );
