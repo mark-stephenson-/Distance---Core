37939f177bc9982ef5178289c5e3bc35dfa9133e
Craig Sansam <craig@craigy.co.uk>
2013-08-14 14:57:41 +0100

    Ensuring application_id is required

--- app/Core/Validators/Collection.php
+++ app/Core/Validators/Collection.php
@@ -6,6 +6,7 @@ class Collection extends Validator
     public static $rules = array(
         'name'         => 'required',
         'api_key'      => 'required',
+        'application_id' => 'required',
     );
 
     public static $messages = array();
