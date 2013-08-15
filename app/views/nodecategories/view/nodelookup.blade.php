b0f2fa16bc8098175e31206c745faf1635395fa7
Craig Sansam <craig@craigy.co.uk>
2013-08-15 12:44:51 +0100

    Fixing node issue

--- app/views/nodecategories/view/nodelookup.blade.php
+++ app/views/nodecategories/view/nodelookup.blade.php
@@ -1,11 +1,9 @@
 <?php
     $column_name = $data->{$column->name};
+?>
 
-    if ( $column_name ) {
-?>
-    <a href="{{ route('nodes.view', array($column_name)) }}">{{ @Node::find($column_name)->title }}</a>
-<?php
-    } else {
-        echo 'N/A';
-    }
-?>
+@if ( $column_name )
+    <a href="{{ route('nodes.view', array(Request::segment(2),$column_name)) }}">{{ @Node::find($column_name)->title }}</a>
+@else
+    N/A
+@endif
