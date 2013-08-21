2190caf9ce8960b0142cfc36f342b003380ce094
Sam Jordan <sam@netsells.co.uk>
2013-08-21 13:41:59 +0100

    /modules fix

--- app/controllers/Api/ModulesController.php
+++ app/controllers/Api/ModulesController.php
@@ -7,10 +7,11 @@ class ModulesController extends \BaseController {
     
     public function modules()
     {
-        Input::replace(array(
+        Input::replace(array_merge(array(
             'nodeType'          => '14',
             'expandChildNodes'  => true,
-        ));
+        ), Input::all()));
+
         return App::make('Api\NodeController')->nodes();
     }
 }
