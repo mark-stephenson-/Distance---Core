36aa52bfb2e808014776a735c489f9574002cae1
Sam Jordan <sam@netsells.co.uk>
2013-10-06 15:52:36 +0100

    Fixing collection switching issue

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -6,6 +6,14 @@ class BaseController extends Controller {
         $appId = Request::segment(2);
         if (is_numeric($appId) and Request::segment(1) == 'apps') {
             Session::put('current-app', $appId);
+
+            $collectionId = Request::segment(4);
+            if (is_numeric($collectionId) and Request::segment(3) == 'collections') {
+                Session::put('current-collection', $collectionId);
+            } else {
+                // This stores a guessed app ID
+                Collection::currentId();
+            }
         } else {
             // This stores a guessed app ID
             Application::currentId();
