b039b9d93db6fa84ac6d55ea83b75a9a1b0d43ff
Sam Jordan <sam@netsells.co.uk>
2013-10-10 22:04:03 +0100

    Added hierarchy method guts to API

--- app/controllers/Api/HierarchyController.php
+++ app/controllers/Api/HierarchyController.php
@@ -10,16 +10,37 @@ class HierarchyController extends \BaseController {
         if ( Request::header('Collection-Token') === NULL ) {
             return Response::make('Collection-Token must be specified for this call.', 400);
         }
+        
+        Input::replace(array_merge(array(
+            'headersOnly'  => 'yes',
+        ), Input::all()));
+
+        $nodeData = App::make('Api\NodeController')->nodes(false);
+        $nodes = array();
+
+        foreach($nodeData as $node) {
+            $nodes[$node['id']] = $node;
+        }
 
         $collection = Collection::find(App::make('collection')->id);
 
         $branches = $collection->hierarchy;
         $branches->findChildren();
 
-        if ( Input::get('withNodeData') ) {
-            
+        $return = $this->buildHierarchyLevel($branches, $nodes);
+
+        return Api::makeResponse($return, 'hierarchy');
+    }
+
+    protected function buildHierarchyLevel($branches, $nodeData) {
+
+        $return = array();
+
+        foreach($branches->getChildren() as $branch) {
+            $return[] = array_merge($nodeData[$branch->node_id], array('branches' => $this->buildHierarchyLevel($branch, $nodeData)));
         }
 
-        return Api::makeResponse($branches, 'hierarchy');
+        return $return;
+
     }
 }
