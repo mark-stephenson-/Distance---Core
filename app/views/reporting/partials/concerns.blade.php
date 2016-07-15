061d0774e595d08c5074e75310909372f0c93e65
Jack Colley <jack.colley@netsells.co.uk>
2016-07-15 16:02:36 +0100

    Adding drop downs on concerns tab

--- app/views/reporting/partials/concerns.blade.php
+++ app/views/reporting/partials/concerns.blade.php
@@ -1,3 +1,17 @@
+{{ Form::open(['method' => 'GET', 'class' => 'form-inline']) }}
+<div class="span4">
+    {{ Form::label('filter', 'Primary Filter') }}
+    {{ Form::select('filter', ['preventability' => 'Preventability', 'severity' => 'Severity']) }}
+</div>
+<div class="span4">
+    {{ Form::label('preventability', 'Preventability Order') }}
+    {{ Form::select('preventability', ['not' => 'Not Preventable', 'maybe' => 'May be preventable', 'preventable' => 'Preventable', 'unknown' => 'Unknown (Don\'t know'], 'not') }}
+</div>
+<div class="span4">
+    {{ Form::label('severity', 'Severity Order') }}
+    {{ Form::select('severity', ['low' => '1 - 3 Low', 'medium' => '4 - 6 Medium', 'high' => '7 - 10 High'], 'low') }}
+</div>
+{{ Form::close() }}
 <table class="table">
     <thead>
     <th>Patient Concern</th>
@@ -5,7 +19,9 @@
     <th>Severity</th>
     </thead>
     <tbody>
+
     @foreach($concerns as $concern)
+
         <tr>
             <td>{{ $concern->text }}</td>
             <td>
