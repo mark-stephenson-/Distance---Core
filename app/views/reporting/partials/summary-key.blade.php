c0a2808f5e8b237bc573d57b5e67f9f010e7e93f
Sam Jordan <sam@netsells.co.uk>
2016-07-19 14:37:31 +0100

    Added padding
    PRASE-50

--- app/views/reporting/partials/summary-key.blade.php
+++ app/views/reporting/partials/summary-key.blade.php
@@ -1,31 +1,31 @@
-<table width="100%">
+<table width="100%" style="margin-bottom: 15px;">
     <tr>
         <th width="5%">Key</th>
-        <td width="13.5%">
+        <td width="11%">
             <span class="colour key-danger"></span>
             Negative-
         </td>
-        <td width="13.5%">
-            <span class="colour key-danger"></span>
-            Negative-
-        </td>
-        <td width="13.5%">
+        <td width="11%">
             <span class="colour key-warning"></span>
             Negative
         </td>
-        <td width="13.5%">
+        <td width="11%">
             <span class="colour key-neutral"></span>
             Neutral
         </td>
-        <td width="13.5%">
+        <td width="11%">
             <span class="colour key-positive"></span>
+            Positive
+        </td>
+        <td width="11%">
+            <span class="colour key-success"></span>
             Positive+
         </td>
-        <td width="13.5%">
+        <td width="15%">
             <i class="icon-check"></i> Something Good
         </td>
-        <td width="13.5%">
-            <i class="icon-exclamation"></i> Concern
+        <td width="15%">
+            <i class="icon-exclamation"></i> Concerns
         </td>
     </tr>
 </table>
