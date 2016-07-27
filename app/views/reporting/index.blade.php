adfacde1844d67e837f7d762d292a46b205508c0
Catalin <catalin.radulescu@netsells.co.uk>
2016-07-27 14:00:42 +0100

    PRASE-71 / PRASE-72 / PRASE-73

--- app/views/reporting/index.blade.php
+++ app/views/reporting/index.blade.php
@@ -13,23 +13,41 @@
     <div class="reports-form">
         {{ Form::open(['id' => 'report-form']) }}
             <div class="span9">
-                <div class="control-group">
+                <div class="row">
 
                     {{ Form::label('period_start', 'Report Period', array('class' => 'control-label span2')) }}
 
                     <div class="controls">
-                        {{ Form::text('period_start', Input::old('period_start'), array('class' => 'span4', 'id' => 'period_start')) }}
-                        {{ Form::text('period_end', Input::old('period_end'), array('class' => 'span4', 'id' => 'period_end')) }}
+                        <div class="input-group span4">
+                            <i class="icon-calendar"></i>
+                            {{ Form::text('period_start', Input::old('period_start'), array('class' => 'span12', 'id' => 'period_start')) }}
+                        </div>
+                        <div class="input-group span4">
+                            <i class="icon-calendar"></i>
+                            {{ Form::text('period_end', Input::old('period_end'), array('class' => 'span12', 'id' => 'period_end')) }}
+                        </div>
                     </div>
                 </div>
-                <div class="control-group">
+                <div class="row">
                     {{ Form::label('trust', 'Location', array('class' => 'control-label span2')) }}
                     <div class="controls">
-                        {{ Form::select('trust', $trusts, Input::old('trust'), array('class' => 'span3')) }}
-                        {{ Form::select('hospital', array(), Input::old('hospital'), array('class' => 'span3', 'style' => 'display: none;')) }}
-                        {{ Form::hidden('wards', null, ['id' => 'wards']) }}
+                        <div class="input-group span4">
+                            {{ Form::select('trust', $trusts, Input::old('trust'), array('class' => 'span12')) }}
+                        </div>
+                        <div class="input-group span4">
+                            {{ Form::select('hospital', array(), Input::old('hospital'), array('class' => 'span12', 'style' => 'display: none;')) }}
+                        </div>
                     </div>
                 </div>
+                <div class="row wards-row hide">
+                    {{ Form::label('wards', 'Wards', array('class' => 'control-label span2')) }}
+                    <div class="controls">
+                        <div class="input-group span4">
+                            {{ Form::hidden('wards', null, ['id' => 'wards', 'class' => 'span12']) }}
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="span2">
                 {{ Form::submit('Generate Report', array('class' => 'submit-button btn', 'id' => 'generate', 'disabled')) }}
@@ -93,6 +111,7 @@
             $('[name=hospital]').on('change', function() {
                 var hospitalId = $(this).val();
 
+                $('.wards-row').show();
                 $("#wards").select2("destroy");
 
                 $('#wards').select2({
