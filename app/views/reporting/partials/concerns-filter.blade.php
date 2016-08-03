7125137e0cb4b3ab2bcae7b255a1655c287555be
Catalin <catalin.radulescu@netsells.co.uk>
2016-08-03 15:34:20 +0100

    PRASE-76 - added alert text instead of showing empty rows

--- app/views/reporting/partials/concerns-filter.blade.php
+++ app/views/reporting/partials/concerns-filter.blade.php
@@ -1,14 +1,16 @@
+<div class="clearfix"></div>
 {{ Form::open(['method' => 'GET', 'class' => 'form-inline filter-form']) }}
-<div class="span4">
-    {{ Form::label('filter', 'Primary Filter') }}
-    {{ Form::select('filter', ['preventability' => 'Preventability', 'severity' => 'Severity'], Input::get('filter')) }}
-</div>
-<div class="span4">
-    {{ Form::label('preventability', 'Preventability Order') }}
-    {{ Form::select('preventability', ['asc' => 'Hard to Easy', 'desc' => 'Easy to Hard'], Input::get('preventability')) }}
-</div>
-<div class="span4">
-    {{ Form::label('severity', 'Severity Order') }}
-    {{ Form::select('severity', ['asc' => 'Low to High', 'desc' => 'High to Low'], Input::get('severity')) }}
-</div>
+    <div class="span4">
+        {{ Form::label('filter', 'Primary Filter') }}
+        {{ Form::select('filter', ['preventability' => 'Preventability', 'severity' => 'Severity'], Input::get('filter')) }}
+    </div>
+    <div class="span4">
+        {{ Form::label('preventability', 'Preventability Order') }}
+        {{ Form::select('preventability', ['asc' => 'Hard to Easy', 'desc' => 'Easy to Hard'], Input::get('preventability')) }}
+    </div>
+    <div class="span4">
+        {{ Form::label('severity', 'Severity Order') }}
+        {{ Form::select('severity', ['asc' => 'Low to High', 'desc' => 'High to Low'], Input::get('severity')) }}
+    </div>
 {{ Form::close() }}
+<br>
--- app/views/reporting/partials/concerns.blade.php
+++ app/views/reporting/partials/concerns.blade.php
@@ -1,48 +1,52 @@
-<table class="table">
-    <thead>
-        <tr>
-            @if (isset($concerns->first()->question))
-                <th>Question</th>
-            @endif
-            <th>Patient Concern</th>
-            <th>Preventability</th>
-            <th>Severity</th>
-        </tr>
-    </thead>
-    <tbody>
+@if(!$concerns->isEmpty())
+    <table class="table">
+        <thead>
+            <tr>
+                @if (isset($concerns->first()->question))
+                    <th>Question</th>
+                @endif
+                <th>Patient Concern</th>
+                <th>Preventability</th>
+                <th>Severity</th>
+            </tr>
+        </thead>
+        <tbody>
 
-    @foreach($concerns as $concern)
+        @foreach($concerns as $concern)
 
-        <tr>
-            @if (isset($concern->question))
-                @if (isset($noLimit))
-                    <td>{{ $concern->question}}</td>
-                @else
-                    <td data-toggle="tooltip" data-placement="top" title="{{ $concern->question }}">{{ (!isset($noLimit)) ? str_limit($concern->question, 20) : $concern->question }}</td>
+            <tr>
+                @if (isset($concern->question))
+                    @if (isset($noLimit))
+                        <td>{{ $concern->question}}</td>
+                    @else
+                        <td data-toggle="tooltip" data-placement="top" title="{{ $concern->question }}">{{ (!isset($noLimit)) ? str_limit($concern->question, 20) : $concern->question }}</td>
+                    @endif
                 @endif
-            @endif
-            <td>{{ $concern->text }}</td>
-            <td>
-                @if($concern->preventability < 2)
-                    Not Preventable
-                @elseif($concern->preventability == 2)
-                    May be preventable
-                @elseif($concern->preventability == 3)
-                    Preventable
-                @else
-                    Unknown
-                @endif
-            </td>
-            <td>
-                @if($concern->severity < 4)
-                    <p style="color: #59af59">Low</p>
-                @elseif($concern->severity >= 4 && $concern->severity <7)
-                    <p style="color: #f9a124">Medium</p>
-                @else
-                    <p style="color: #e45752">High</p>
-                @endif
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+                <td>{{ $concern->text }}</td>
+                <td>
+                    @if($concern->preventability < 2)
+                        Not Preventable
+                    @elseif($concern->preventability == 2)
+                        May be preventable
+                    @elseif($concern->preventability == 3)
+                        Preventable
+                    @else
+                        Unknown
+                    @endif
+                </td>
+                <td>
+                    @if($concern->severity < 4)
+                        <p style="color: #59af59">Low</p>
+                    @elseif($concern->severity >= 4 && $concern->severity <7)
+                        <p style="color: #f9a124">Medium</p>
+                    @else
+                        <p style="color: #e45752">High</p>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@else
+    <div class="alert alert-success">No specific patient experience was reported relating to these questions</div>
+@endif
