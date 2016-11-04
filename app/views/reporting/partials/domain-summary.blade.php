804e93e1ae8fcc4f43ba39f33c451feaf7a97d0a
Catalin <catalin.radulescu@netsells.co.uk>
2016-11-04 11:01:12 +0000

    division by zero error

--- app/views/reporting/partials/domain-summary.blade.php
+++ app/views/reporting/partials/domain-summary.blade.php
@@ -17,19 +17,26 @@
             <td>
                 <div class="progress">
                     <?php
-                        $total = 1;
+                        $total = 0;
                         foreach($domain->summary as $key => $val) {
                             if ($key > 0) {
                                 $total += $val;
                             }
                         }
                     ?>
-                    <div class="bar bar-danger" style="width: {{ floor(($domain->summary->{"1"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Negative-: {{ $domain->summary->{"1"} }}"> </div><!--
-                    !--><div class="bar bar-warning" style="width: {{ floor(($domain->summary->{"2"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Negative: {{ $domain->summary->{"2"} }}"> </div><!--
-                    !--><div class="bar bar-neutral" style="width: {{ floor(($domain->summary->{"3"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Neutral: {{ $domain->summary->{"3"} }}"> </div><!--
-                    !--><div class="bar bar-positive" style="width: {{ floor(($domain->summary->{"4"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Positive: {{ $domain->summary->{"4"} }}"> </div><!--
-                    !--><div class="bar bar-success" style="width: {{ floor(($domain->summary->{"5"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Positive+: {{ $domain->summary->{"5"} }}"> </div><!--
-                    !-->
+                    @if($total > 0)
+                        <div class="bar bar-danger" style="width: {{ floor(($domain->summary->{"1"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Negative-: {{ $domain->summary->{"1"} }}"> </div>
+                        <div class="bar bar-warning" style="width: {{ floor(($domain->summary->{"2"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Negative: {{ $domain->summary->{"2"} }}"> </div>
+                        <div class="bar bar-neutral" style="width: {{ floor(($domain->summary->{"3"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Neutral: {{ $domain->summary->{"3"} }}"> </div>
+                        <div class="bar bar-positive" style="width: {{ floor(($domain->summary->{"4"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Positive: {{ $domain->summary->{"4"} }}"> </div>
+                        <div class="bar bar-success" style="width: {{ floor(($domain->summary->{"5"}/$total) * 100) }}%;" data-toggle="tooltip" data-placement="top" title="Positive+: {{ $domain->summary->{"5"} }}"> </div>
+                    @else
+                        <div class="bar bar-danger" style="width: 0%;" data-toggle="tooltip" data-placement="top" title="Negative-: {{ $domain->summary->{"1"} }}"> </div>
+                        <div class="bar bar-warning" style="width: 0%;" data-toggle="tooltip" data-placement="top" title="Negative: {{ $domain->summary->{"2"} }}"> </div>
+                        <div class="bar bar-neutral" style="width: 0%;" data-toggle="tooltip" data-placement="top" title="Neutral: {{ $domain->summary->{"3"} }}"> </div>
+                        <div class="bar bar-positive" style="width: 0%;" data-toggle="tooltip" data-placement="top" title="Positive: {{ $domain->summary->{"4"} }}"> </div>
+                        <div class="bar bar-success" style="width: 0%;" data-toggle="tooltip" data-placement="top" title="Positive+: {{ $domain->summary->{"5"} }}"> </div>
+                    @endif
                 </div>
             </td>
             <td>
